fe166f8ee1478c756c3b48092fd571dc6619c001
Andrew Tam <andrew.tam.000@gmail.com>
2017-06-11 11:35:30 -0400

    Dispatch is undefined

--- src/app/containers/monitors/StoreModifierWrapper.js
+++ src/app/containers/monitors/StoreModifierWrapper.js
@@ -4,7 +4,7 @@ import StoreModifier from 'redux-devtools-store-modifier';
 const StoreModifierWrapper = ({computedStates, currentStateIndex, dispatchFn }) => (
     <StoreModifier
         state={ computedStates.length ? computedStates[currentStateIndex].state : {one: 'teo'}}
-        dispatch={ dispatch || this.context.store.dispatch }
+        dispatch={ dispatchFn || this.context.store.dispatch }
     />
 )
 
