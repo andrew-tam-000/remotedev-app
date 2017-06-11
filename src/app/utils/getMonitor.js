53237f37ba49d911f40b46d25d00fa7213cb6b23
Andrew Tam <andrew.tam.000@gmail.com>
2017-06-11 11:11:43 -0400

    Adding store modifier

--- src/app/utils/getMonitor.js
+++ src/app/utils/getMonitor.js
@@ -2,8 +2,10 @@ import React, { Component, PropTypes } from 'react';
 import LogMonitor from 'redux-devtools-log-monitor';
 import ChartMonitorWrapper from '../containers/monitors/ChartMonitorWrapper';
 import InspectorWrapper from '../containers/monitors/InspectorWrapper';
+import StoreModifier from 'redux-devtools-store-modifier';
 
 export const monitors = [
+  { key: 'StoreModifier', title: 'Store Modifier' },
   { key: 'LogMonitor', title: 'Log monitor' },
   { key: 'InspectorMonitor', title: 'Inspector' },
   { key: 'ChartMonitor', title: 'Chart' }
@@ -11,6 +13,8 @@ export const monitors = [
 
 export default function getMonitor({ monitor, lib }) {
   switch (monitor) {
+    case 'StoreModifier':
+      return <StoreModifier/>;
     case 'LogMonitor':
       return <LogMonitor preserveScrollTop={false} markStateDiff />;
     case 'ChartMonitor':
