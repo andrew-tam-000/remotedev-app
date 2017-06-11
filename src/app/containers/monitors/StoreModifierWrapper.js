1d06f0e89289b16d77b6a6c8491c746acb6ee9e9
Andrew Tam <andrew.tam.000@gmail.com>
2017-06-11 11:38:06 -0400

    Updating context reference

--- src/app/containers/monitors/StoreModifierWrapper.js
+++ src/app/containers/monitors/StoreModifierWrapper.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import StoreModifier from 'redux-devtools-store-modifier';
 
-const StoreModifierWrapper = ({computedStates, currentStateIndex, dispatchFn }) => (
+
+const StoreModifierWrapper = ({computedStates, currentStateIndex, dispatchFn}, context) => (
     <StoreModifier
         state={ computedStates.length ? computedStates[currentStateIndex].state : {one: 'teo'}}
-        dispatch={ dispatchFn || this.context.store.dispatch }
+        dispatch={ dispatchFn || context.store.dispatch }
     />
 )
+
+StoreModifierWrapper.contextTypes = {
+    store: React.PropTypes.object
+};
 
 export default StoreModifierWrapper;
